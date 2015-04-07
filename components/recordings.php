107efc579650a27a45aa256506f8042c3c921d4a
Roman Khomasuridze <khomasuridze@gmail.com>
2015-04-07 02:42:04 -0400

    we now display cause editting form

--- components/recordings.php
+++ components/recordings.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
 Copyright (c) 2014-2015, Roman Khomasuridze, (khomasuridze@gmail.com)
 All rights reserved.
@@ -36,13 +35,53 @@ include "shared.php";
 // get list of recordings and draw rnav
 // of course we could name this files cause.php, and whole section whould be cause management, which would be, em, not pretty
 // anyways above can be done bit later
+$cause_list = fbilling_get_list('causes');
+echo "<div class='rnav'><ul>";
+foreach ($cause_list as $cause) {
+    echo "<li class='current'><a href=/admin/config.php?display=$display&cat=$cat&action=edit&id=$cause[id]>$cause[name]</a></li>";
+}
+echo "</ul></div>";
+$recording_list = recordings_list();
 
-$component_list = fbilling_get_list('causes');
-	echo "<div class='rnav'><ul>";
-	echo "<li class='current'><a href=/admin/config.php?display=$display&cat=$cat&action=add>Add $fbilling_strings[$cat]</a></li>";
-	foreach ($component_list as $component) {
-	    echo "<li class='current'><a href=/admin/config.php?display=$display&cat=$cat&action=edit&id=$component[id]>$component[name]</a></li>";
-	}
-	echo "</ul></div>";
 
+
+if ($action == 'edit') { // start edit cause
+	$cause_data = fbilling_get_data_by_id('causes',$id);
+
+?>
+
+<form name='cause' method='GET'>
+    <table>
+        <tr>
+            <td>
+                <a href='#' class='info'><?php echo _("Cause Name"); ?><span><?php echo _("Selected recording will be played whenever extension will hit that cause"); ?></span></a></td>
+            </td>
+            <td>
+                <input type='text' name='name' disabled=true tabindex="<?php echo ++$tabindex;?>" <?php if ($action == 'edit') {echo "value='".$cause_data['name']."'";} ?> >
+            </td>
+        </tr>
+        <tr>
+        	<td>
+                <a href='#' class='info'><?php echo _("Recording"); ?><span><?php echo _("Select Recording that will be played back when this cause is hit"); ?></span></a></td>
+            </td>
+        	<td>
+        		<select name='recording' tabindex="<?php echo ++$tabindex;?>">
+    				<option selected value="0"><?php echo _("None"); ?></option>
+    				<?php
+    					foreach ($recording_list as $rec) {
+    						echo "<option value=$rec[id]>$rec[displayname]</option>";
+    					}
+    				?>
+    			</select>
+    		</td>
+        </tr>
+    </table>
+    <input type='hidden' name='display' value=<?php echo $display; ?>>
+    <input type='hidden' name='action' value=<?php echo $form_action; ?>>
+    <?php if ($action == 'edit') {echo "<input type='hidden' name='id' value=$id>";} ?>
+    <input type='hidden' name='cat' value=<?php echo $cat; ?> >
+    <td colspan="2"><br><h6><input name="submit" type="submit" value="<?php echo _("Submit")?>" tabindex="<?php echo ++$tabindex;?>"></h6></td>
+</form>
+<?php
+} // end edit cause
 ?>
