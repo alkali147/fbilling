3edca3a6f3f62756276eb04047b54a39dc8c9e0f
Roman Khomasuridze <khomasuridze@gmail.com>
2016-01-20 09:39:15 +0400

    added new hangup cause

--- upgrade/upgrade.sql
+++ upgrade/upgrade.sql
@@ -20,5 +20,6 @@ CREATE TABLE IF NOT EXISTS `billing_payments` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 ALTER TABLE asterisk.billing_tenants ADD COLUMN billing_tenants.credit float NOT NULL;
 INSERT INTO billing_causes (`id`,`name`,`recording_id`) VALUES ('14','Tenant has no credit','0');
+INSERT INTO billing_causes (`id`,`name`,`recording_id`) VALUES ('15','Account is not assigned to any tenant','0');
 ALTER TABLE billing_extensions ADD COLUMN personal_credit int(1) DEFAULT '1';
 
